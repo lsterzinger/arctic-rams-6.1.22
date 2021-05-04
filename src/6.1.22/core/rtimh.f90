dbbb6c058b3c9e7bc82ff85327d9b2d1df41bdb7
Lucas Sterzinger <lsterzinger@ucdavis.edu>
2021-05-04 08:40:36 -0700

    Removed condition that iforceccn != 0

--- src/6.1.22/core/rtimh.f90
+++ src/6.1.22/core/rtimh.f90
@@ -53,7 +53,7 @@ integer :: callmassflux,massfluxfreq
 !    basic_g(ngrid)%thp(1,1,1),&
 !    basic_g(ngrid)%theta(1,1,1))
 
-if ((itempnudge.ne.0) .and. (time < fccnstart) .and. (iforceccn.ne.0)) then
+if ((itempnudge.ne.0) .and. (time < fccnstart)) then
    ! print *, "Nudging temperature to initial sounding"
    CALL temp_adj(mzp,mxp,myp,&
       basic_g(ngrid)%thp(1,1,1))
